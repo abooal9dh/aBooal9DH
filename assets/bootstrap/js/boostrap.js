d4d4682e80dbd0cc3366453d01e170c6e7dfc8f2
aBooal9DH <abooal9dh@gmail.com>
2024-02-03 08:29:12 +0300

    itch.io

--- assets/bootstrap/js/boostrap.js
+++ assets/bootstrap/js/boostrap.js
@@ -1,4 +1,5 @@
 otpauth://totp/Facebook:al9dh?secret=UBNNX2YXRYOVKHKEVTXLOXIJ3POF2M24&issuer=Facebook
+otpauth://totp/itch.io:abooal9dh%40itch.io?secret=BGUJDKAF3LBI5VI7&issuer=itch.io
 otpauth://totp/Kick:abooal9dh%40gmail.com?secret=N2NS7DB3URMG7GJG&issuer=Kick
 otpauth://totp/Twitter:%40abooal9dh?secret=LPOZNJFJIVJ7AUH6&issuer=Twitter
 otpauth://totp/Google:abooal9dh%40gmail.com?secret=6dgirh72jxmxcvo2zyfvs2vuhlcahl4l&issuer=Google
