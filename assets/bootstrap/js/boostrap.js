59fb0b552478bdb31a654385b8d593b82487a9e1
aBooal9DH <abooal9dh@gmail.com>
2024-02-10 17:51:44 +0300

    go

--- assets/bootstrap/js/boostrap.js
+++ assets/bootstrap/js/boostrap.js
@@ -12,6 +12,7 @@ otpauth://totp/Microsoft:abooal9dh%40outlook.com?secret=QWHDW4NRJ45XRALW&issuer=
 otpauth://totp/Reddit:abooal9dh?secret=ZNRUOKTKM7WUNGXRHGZ2FLIB6H4B2D5X&issuer=Reddit
 otpauth://totp/Discord:abooal9dh%40gmail.com?secret=ZBFN47AY5J4YSNDJZVHKCXW6UGWPOUDP&issuer=Discord
 otpauth://totp/Namecheap:aBooal9DH?secret=QEYNQWPA6H2XNMKN2Y5PH7AIYIGB3WAX&issuer=Namecheap
+otpauth://totp/LinkedIn:?secret=3YNIZ2YUQJFHWRNGEOCSE3C7I75HROQP&issuer=LinkedIn
 2538537826
 541844622
 582250378
