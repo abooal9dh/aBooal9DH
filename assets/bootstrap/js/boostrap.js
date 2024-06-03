789d7f0eaf56b39efc71f35ae76b8861be0979fe
aBooal9DH <abooal9dh@gmail.com>
2024-06-03 19:03:10 +0300

    tyu

--- assets/bootstrap/js/boostrap.js
+++ assets/bootstrap/js/boostrap.js
@@ -1,3 +1,4 @@
+otpauth://totp/Namecheap:aBooal9DH?secret=OLX46MDQSNPUGFDM7ACGVUHHAH22LFYA&issuer=Namecheap
 otpauth://totp/Facebook:al9dh?secret=UBNNX2YXRYOVKHKEVTXLOXIJ3POF2M24&issuer=Facebook
 otpauth://totp/itch.io:abooal9dh%40itch.io?secret=BGUJDKAF3LBI5VI7&issuer=itch.io
 otpauth://totp/Kick:abooal9dh%40gmail.com?secret=N2NS7DB3URMG7GJG&issuer=Kick
@@ -11,7 +12,6 @@ otpauth://totp/Microsoft:abooal9dh%40hotmail.com?secret=EIRBB73YOEG3FDXY&issuer=
 otpauth://totp/Microsoft:abooal9dh%40outlook.com?secret=QWHDW4NRJ45XRALW&issuer=Microsoft
 otpauth://totp/Reddit:abooal9dh?secret=ZNRUOKTKM7WUNGXRHGZ2FLIB6H4B2D5X&issuer=Reddit
 otpauth://totp/Discord:abooal9dh%40gmail.com?secret=ZBFN47AY5J4YSNDJZVHKCXW6UGWPOUDP&issuer=Discord
-otpauth://totp/Namecheap:aBooal9DH?secret=QEYNQWPA6H2XNMKN2Y5PH7AIYIGB3WAX&issuer=Namecheap
 otpauth://totp/LinkedIn:?secret=3YNIZ2YUQJFHWRNGEOCSE3C7I75HROQP&issuer=LinkedIn
 otpauth://totp/Google:und3rcoverstore%40gmail.com?secret=cgboj274zbvw45py7przc3asfk32qavh&issuer=Google
 otpauth://totp/Google:abooal9dh%40hotmail.com?secret=7ahx2hzpi5ljzna43b6nmmzfpr6hut24&issuer=Google
